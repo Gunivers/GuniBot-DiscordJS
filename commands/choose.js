fb8381a3a430c53b24290ca9656ff6b15203d68f
Evan91380 <32957194+Evan91380@users.noreply.github.com>
2018-07-17 16:32:48 +0200

    Update choose.js

--- commands/choose.js
+++ commands/choose.js
@@ -1,23 +1,34 @@
+const Discord = require('discord.js');
 exports.run = (client, message, args, member) => {
+	
+  let split = ';';
+  
+  args = args.join(' ').split(split);
+  
+  for (var i = 0; i < args.length; i++) args[i] = args[i].trim()
+	
+if(!args[0]) return message.channel.send("je ne peut pas creer de choix vide! syntaxe : `/choose choix1 ; choix2 ..... choixX` (5 choix max)")
 
-  if(!args[0] || !args[1]) return message.channel.send(`Merci de préciser vos choix. Usage : \`${message.settings.prefix}choose [choix 1] [choix 2]\``);
+	  if(!args[0]) return message.reply("Tu doit mettre des choix!")
+		  if(!args[1]) return message.reply("Tu doit mettre 2 choix minimum!")
+  
+var choix = `J'ai du choisir entre:`
+ for (var i = 0; i < args.length; i++) {
+	 var diffchoix = []
+	 diffchoix.push(args[i])
+	 choix = choix + `\n${args[i]}` 
+ }
+  
+  var member = message.guild.members.get(message.author.id)
+  
+	const embed = new Discord.RichEmbed()
+			.setColor(member.displayHexColor)
+			.setDescription(choix + `\n\nJ'ai choisi: ${diffchoix[Math.floor(Math.random() * diffchoix.length)]}`)
+			.setFooter("Demande de " + message.author.tag)
+			.setTimestamp()
 
-    var random = Math.floor((Math.random() * 100) + 1);
-
-    if (random <= 46) {
-        random = args[0];
-    } else if (random <= 90) {
-        random = args[1];
-    } else if (random >= 91) {
-		random = "de laisser le dieu <@250279569754423297> répondre à ma place.";
-	}
-    message.channel.send({
-        embed: {
-            color: 0xFF0000,
-            title: message.member.displayName + " m'a demandé de faire un choix entre " + args[0] + " et " + args[1],
-           description: "j'ai choisi : " + random,
-       }
-   })
+	
+	message.channel.send(embed);
 }
 
 exports.conf = {
@@ -32,5 +43,9 @@ exports.help = {
   name: "choose",
   category: "Divers",
   description: "Vous n'arrivez pas à faire un choix ? Demandez au bot !",
-  usage: "choose [choix 1] [choix 2]"
+  usage: "choose nbrchoix ; [choix 1] ; [choix 2]"
 };
+
+function isNumeric(val) {
+  return Number(parseFloat(val)) === val;
+}
